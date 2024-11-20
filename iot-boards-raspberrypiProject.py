4647226129e9004cba6b442c8139bbcd5a38e0d4
Thamoghn <thamoghntummapudi@gmail.com>
2024-11-20 22:41:54 +0530

    Update iot-boards-raspberrypiProject.py

--- iot-boards-raspberrypiProject.py
+++ iot-boards-raspberrypiProject.py
@@ -1,64 +1,39 @@
-import time
+import http.client, urllib
 import RPi.GPIO as GPIO
-import httplib, urllib
+import time
 
-# setup GPIO using Broadcom SOC channel numbering
+APP_TOKEN="aty4pcgs4pn96zkhm8sdi3d1qvziry"
+USER_KEY="ubim8tzcrc9iwt9woin8b7t8abqmk3"
+
+MQ135pin=14
+alert=15
+stpbtn=18
+
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(MQ135pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(alert, GPIO.OUT)
+GPIO.setup(stpbtn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.output(alert,0)
+print("Setup done")
+GPIO.output(alert,1)
+time.sleep(1)
+GPIO.output(alert,0)
 
-# define the GPIO port you will use for the motion detector
-PIR_SENSOR = 19 
-
-# define the GPIO port you will use for the status LED
-# the LED will light up when motion is detected by the PIR sensor
-LED = 13
-
-# number of seconds to delay between alarms
-DELAY = 10
-
-# set to pull-up (normally closed position for a PIR sensor dry contact)
-GPIO.setup(PIR_SENSOR, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# set the LED GPIO to output
-GPIO.setup(LED, GPIO.OUT)
-
-# Pushover API setup
-PUSH_TOKEN = "aty4pcgs4pn96zkhm8sdi3d1qvziry" # API Token/Key
-PUSH_USER = "ubim8tzcrc9iwt9woin8b7t8abqmk3" # Your User Key
-PUSH_MSG = "Motion detected!" # Push Message you want sent
-
-# This function sends the push message using Pushover.
-# Pass in the message that you want sent
-def sendPush( msg ):
-	conn = httplib.HTTPSConnection("api.pushover.net:443")
-	conn.request("POST", "/1/messages.json",
-		urllib.urlencode({
-			"token": PUSH_TOKEN,
-			"user": PUSH_USER,
-			"message": msg,
-		}), { "Content-type": "application/x-www-form-urlencoded" })
-
-	conn.getresponse()
-	return
-
-try:
-	# setup an indefinite loop that looks for the PIR sensor to be triggered
-	while True:
-		# motion is detected
-		GPIO.wait_for_edge(PIR_SENSOR, GPIO.RISING)
-		# light LED
-		GPIO.output(LED, True)
-		# print and push message
-		print(PUSH_MSG)
-		sendPush(PUSH_MSG)
-
-		# do you want a time delay in between alarms?
-		time.sleep(DELAY)
-		# after delay, turn LED off
-		GPIO.output(LED, False)
-
-except KeyboardInterrupt:
-	# cleanup GPIOs on keyboard exit
-	GPIO.cleanup()
-
-# cleanup GPIOs when program exits
-GPIO.cleanup()
+while True:
+    if GPIO.input(MQ135pin):
+        continue
+    print("Gas Detected")
+    GPIO.output(alert,1)
+    conn = http.client.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json",
+      urllib.parse.urlencode({
+        "token": APP_TOKEN,
+        "user": USER_KEY,
+        "message": "Harmful gas detected!!!",
+      }), { "Content-type": "application/x-www-form-urlencoded" })
+    print("Response: ",conn.getresponse())
+    while GPIO.input(stpbtn):
+        time.sleep(1)
+    GPIO.output(alert,0)
+    print("Alarm reset")
